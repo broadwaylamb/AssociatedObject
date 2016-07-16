6099ddd6379e3adffb338e154ba00a0bb6cf1718
broadwaylamb <seergy_pro@me.com>
2016-07-17 01:01:23 +0300

    Initial Commit

---
+++ AssociatedObject/AssociatedObject.h
@@ -0,0 +1,19 @@
+//
+//  AssociatedObject.h
+//  AssociatedObject
+//
+//  Created by Sergej Jaskiewicz on 17.07.16.
+//  Copyright © 2016 Sergej Jaskiewicz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AssociatedObject.
+FOUNDATION_EXPORT double AssociatedObjectVersionNumber;
+
+//! Project version string for AssociatedObject.
+FOUNDATION_EXPORT const unsigned char AssociatedObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssociatedObject/PublicHeader.h>
+
+
